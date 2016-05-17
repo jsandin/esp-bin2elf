751a7602fd7301d73c5fa6c5834e92effdc7d82f
Joel Sandin <jsandin@gmail.com>
2016-05-17 17:27:45 -0400

    new workflow - user must name all sections before elf generation can succeed.

--- esp_bin2elf.py
+++ esp_bin2elf.py
@@ -8,29 +8,42 @@
 from esp_rom import EspRom
 from esp_elf import XtensaElf, EspElfSection
 
-def prompt_for_section_name():
-    return raw_input("what should this section be called?  If you don't know what the section is for, press enter to pick a generic name (and corresponding values) based on the physical memory region\n>")
+addr_to_section_name_mapping = {}
 
-def convert_rom_to_elf(rom_filename):
+def parse_rom(rom_name, rom_filename):
     with open(rom_filename) as f:
-        rom = EspRom(f)
-        elf = XtensaElf(rom_filename + '.elf', rom.header.entry_addr)
+        rom = EspRom(rom_name, f)
 
-        null_section = EspElfSection('', 0x0, '')
-        elf.add_section(null_section)
+    return rom
 
-        flash_section = EspElfSection('.irom0.text', 0x40200000, rom.contents)
-        elf.add_section(flash_section)
 
-        for section in rom.sections:
-            print section, section.contents
-            section_name = prompt_for_section_name()
-            elf_section = EspElfSection(section_name, section.address, section.contents)
-            elf.add_section(elf_section)
+def name_sections(rom):
+    for section in rom.sections:
+        name = raw_input("enter unique name for 0x%04x>" % (section.address))
+        addr_to_section_name_mapping[section.address] = name
 
-        #elf.add_symtab()
-        elf.generate_string_table()
-        elf.generate_program_header()
-        elf.populate_section_header_metadata()
+
+def convert_rom_to_elf(esp_rom):
+    elf = XtensaElf(esp_rom.name + '.elf', esp_rom.header.entry_addr)
+
+    null_section = EspElfSection('', 0x0, '')
+    elf.add_section(null_section)
+
+    flash_section = EspElfSection('.irom0.text', 0x40200000, esp_rom.contents)
+    elf.add_section(flash_section)
+
+    for section in esp_rom.sections:
+        if section.address not in addr_to_section_name_mapping:
+            print "generation failed: no name for 0x%04x." % (section.address)
+            return None
+
+        name = addr_to_section_name_mapping[section.address]
+        elf_section = EspElfSection(name, section.address, section.contents)
+        elf.add_section(elf_section)
+
+    #elf.add_symtab()
+    elf.populate_section_header_metadata()
+    elf.generate_string_table()
+    elf.generate_program_header()
 
     return elf
