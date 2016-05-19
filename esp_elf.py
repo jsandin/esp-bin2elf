e767aefafcbd8c2f1e9632ad18221c75aa6e5b74
Joel Sandin <jsandin@gmail.com>
2016-05-19 17:57:14 -0400

    properly generate and populate program headers, and calculate all elf offsets for writing.

--- esp_elf.py
+++ esp_elf.py
@@ -2,7 +2,8 @@
 #
 # MIT licence
 
-from elffile import ElfFileIdent, ElfFileHeader32l, ElfFile32l, ElfSectionHeader32l
+from elffile import ElfFileIdent, ElfFileHeader32l, ElfFile32l, ElfSectionHeader32l, ElfProgramHeader32l
+from esp_memory_map import is_code, is_data
 
 class XtensaElf(object):
     def __init__(self, elf_name, entry_addr):
@@ -22,38 +23,76 @@ class XtensaElf(object):
         header.machine = 94          # EM_XTENSA
         header.type = 2              # ET_EXEC
         header.version = 1           # 1
-        header.ehsize = 52           # sizeof(struct elfhdr)
+        header.ehsize = 52           # sizeof(Elf32_Ehdr)
 
         header.shnum = 0             # 0 sections initially
+        header.shentsize = 40        # sizeof(Elf32_Shdr)
+
+        header.phnum = 0             # 0 program headers initially
+        header.phentsize = 32        # sizeof(Elf32_Phdr)
    
         self.elf = ElfFile32l(elf_name, ident)
         self.elf.fileHeader = header
+        self.sections = []
 
-    def generate_program_header(self):
-        self.elf.fileHeader.phentsize = 0
-        self.elf.fileHeader.phnum = 0
-        self.elf.fileHeader.phoff = 0
+    def add_section(self, esp_section, add_to_program_header=False):
+        if(esp_section.header.name == ''):
+            # null section must go in front of section list
+            self.sections.insert(0, esp_section)
+        else:
+            self.sections.append(esp_section)
 
-    def populate_section_header_metadata(self):
-        self.elf.fileHeader.shentsize = 40
-        self.elf.fileHeader.shoff = 0
+        if add_to_program_header:
+            esp_section.generate_program_header()
 
-    def generate_string_table(self):
+    def generate_elf(self):
+        # layout = elfheader | section contents | sheaders | pheaders
+        self._generate_null_section_and_string_table()
+
+        offset = self.elf.fileHeader.ehsize
+
+        for section in self.sections:
+            section.header.offset = offset
+            self.elf.sectionHeaders.append(section.header)
+            self.elf.fileHeader.shnum += 1
+
+            if section.program_header:
+                section.program_header.offset = offset
+                self.elf.programHeaders.append(section.program_header)
+                self.elf.fileHeader.phnum += 1
+
+            offset += section.header.section_size
+
+        self.elf.phoff = offset
+        offset += self.elf.fileHeader.phentsize * self.elf.fileHeader.phnum
+
+        self.elf.shoff = offset
+        offset += self.elf.fileHeader.shentsize  * self.elf.fileHeader.shnum
+
+    def _generate_null_section_and_string_table(self):
+        null_section = EspElfSection('', 0x0, '')
+        self.add_section(null_section, True)
+
+        # its not obvious, but since the null section is first in
+        # the sections list, it a nameoffset of 0 as a result here
         shstrtab_contents = ''
 
+        for section in self.sections:
+            section.header.nameoffset = len(shstrtab_contents)
+            shstrtab_contents += (section.header.name + '\00')
+
+        # need to add the shstrtab name as well
+        nameoffset = len(shstrtab_contents)
+        shstrtab_contents += ('.shstrtab' + '\00')
+
+        # now we can add the shstrtab section
         string_table = EspElfSection('.shstrtab', 0x0, shstrtab_contents)
+        string_table.nameoffset = nameoffset
         self.add_section(string_table)
-        self.elf.fileHeader.shstrndx = len(self.elf.sectionHeaders) - 1
 
-        # its not obvious, but the null section is first in the
-        # headers list, and gets a nameoffset of 0 as a result here.
-        for section in self.elf.sectionHeaders:
-            section.nameoffset = len(shstrtab_contents)
-            shstrtab_contents += (section.name + '\00')
+        # set shstrndx to index of shstrtab section
+        self.elf.fileHeader.shstrndx = len(self.sections) - 1
 
-    def add_section(self, esp_section):
-        self.elf.sectionHeaders.append(esp_section.header)
-        self.elf.fileHeader.shnum = self.elf.fileHeader.shnum + 1
 
     def add_symtab(self):
         pass
@@ -86,6 +125,20 @@ class EspElfSection(object):
         header.flags = settings_to_use.flags
 
         self.header = header
+        self.program_header = None
+
+    def generate_program_header(self):
+        program_header = ElfProgramHeader32l()
+
+        program_header.type = 1     # LOAD
+        program_header.align = 0x1
+        program_header.flags = 6   # fix this
+        program_header.filesz = self.header.section_size
+        program_header.memsz = self.header.section_size
+        program_header.paddr = self.header.addr
+        program_header.vaddr = self.header.addr
+
+        self.program_header = program_header
 
 
 class SectionSettings(object):
